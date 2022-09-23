93075c37f3ce8d5e18ef77b0a2a951a2e5aeef00
rony9843 <rony13647@gmail.com>
2022-09-23 12:49:36 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam
+        maiores asperiores alias unde dolor omnis in quibusdam cumque aliquid,
+        saepe veniam harum esse architecto distinctio at nostrum hic nisi
+        ratione atque sunt similique. Eos voluptatum ullam molestias minus quis
+        adipisci laborum saepe. Totam ipsam, voluptatem non iure voluptas nihil
+        obcaecati velit. Dolorem suscipit provident harum eius, quis velit
+        quaerat! Voluptas dolor earum sit dolore amet porro nihil perspiciatis
+        esse minus beatae obcaecati, reprehenderit sunt aut illo ducimus ut?
+        Quisquam ea ratione aliquid voluptatibus optio at quos atque voluptas!
+        Consequuntur deserunt similique odio, blanditiis quasi ea assumenda,
+        repudiandae, facere eveniet accusamus corrupti expedita optio quidem. In
+        animi veniam repudiandae officia magni aspernatur aliquam quaerat at
+        eius, quia labore deleniti voluptatum adipisci est qui distinctio
+        voluptatem, ad nesciunt voluptatibus saepe numquam quas asperiores. Ut
+        odit, natus laborum fugit beatae reprehenderit minus voluptatum, dolorum
+        omnis architecto nostrum facere eos unde! Numquam id nostrum adipisci
+        sequi quisquam libero dolorum quos totam quod. Voluptas consectetur
+        delectus pariatur quas natus! Nulla quasi earum beatae dolore voluptate
+        labore sed deserunt mollitia dolorum. Saepe, a. Vel rem sunt, veritatis
+        accusantium aliquam dolorem cum, minus accusamus omnis neque eligendi
+        laudantium enim nobis eveniet, laboriosam a. Eligendi, quibusdam sequi?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, velit
+        sapiente magni distinctio voluptate repudiandae excepturi debitis totam
+        sint earum praesentium rem quae consequuntur accusantium. Unde soluta
+        repellendus nesciunt at rerum, ab facere ratione consequatur molestias
+        placeat, aspernatur quo. Perspiciatis excepturi maiores exercitationem
+        molestiae laudantium dignissimos expedita quo aliquid ratione inventore
+        alias, odio architecto debitis voluptate facere mollitia in et porro?
+        Officiis tempora consequatur dignissimos! Necessitatibus iusto quidem
+        itaque nesciunt consectetur aspernatur rem? Ipsam beatae nesciunt atque
+        corporis voluptatum animi distinctio, est iusto laudantium. Aperiam
+        dignissimos atque ea magnam, alias rerum cupiditate deleniti. A ullam
+        quo deleniti. Iure eos obcaecati quasi aliquid minus labore quos
+        praesentium enim dicta suscipit explicabo necessitatibus aspernatur
+        dolorem modi quas mollitia quia, provident veritatis repellendus eius
+        libero ducimus cum commodi? Cupiditate harum ut at molestiae vitae
+        eaque! Quae iusto cupiditate esse eveniet odit. Unde labore
+        consequuntur, eius vitae quibusdam rem maiores aperiam temporibus quidem
+        consequatur, repudiandae, ea animi dignissimos sit accusantium vel
+        eveniet adipisci sint dolorem nesciunt voluptatem minima veritatis.
+        Repellendus qui culpa neque saepe odit consequuntur fuga, reprehenderit
+        exercitationem atque veritatis ad tempore amet provident fugiat eos
+        nesciunt quia dolores a recusandae eveniet mollitia doloribus illo.
+        Voluptas ex repellendus obcaecati inventore repudiandae repellat quos.
+      </div>
     </div>
   );
 }
